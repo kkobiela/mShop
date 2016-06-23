4dd9f547b8426a4ee8c8eac353f4a4a431502f51
HannaHanslik <hannahanslik@gmail.com>
2016-06-23 22:53:24 +0200

    Add tests for ShopModel methods

--- mShopTests/ShopModelTest.cs
+++ mShopTests/ShopModelTest.cs
@@ -60,5 +60,74 @@ namespace mShopTests
             Assert.AreEqual(expectedGoodValue, good);
             Assert.AreEqual(expectedBadValue, bad);
         }
+
+        [TestMethod]
+        public void GetProductsByBrand()
+        {
+            //arrange
+            int expectedGoodValue = 9;
+            int expectedBadValue = 0;
+            string correctProductBrand = "warzywoland";
+            string wrongProductBrand = "zlaMarka";
+            Model model = new Model();
+            string login = "Karol_Bok";
+            string password = "bok";
+            model.Login = login;
+            model.Password = password;
+            model.OpenConnection(ConnectionType.Shop);
+
+            //act
+            int good = model.ShopModel.GetProductsByBrand(correctProductBrand).Count;
+            int bad = model.ShopModel.GetProductsByBrand(wrongProductBrand).Count;
+
+
+            //assert
+            Assert.AreEqual(expectedGoodValue, good);
+            Assert.AreEqual(expectedBadValue, bad);
+        }
+
+        [TestMethod]
+        public void GetProducts()
+        {
+            //arrange
+            int expectedGoodValue = 44;
+            Model model = new Model();
+            string login = "Karol_Bok";
+            string password = "bok";
+            model.Login = login;
+            model.Password = password;
+            model.OpenConnection(ConnectionType.Shop);
+
+            //act
+            int good = model.ShopModel.GetProducts().Count;
+
+            //assert
+            Assert.AreEqual(expectedGoodValue, good);
+        }
+
+        [TestMethod]
+        public void GetProductsByBarcode()
+        {
+            //arrange
+            int expectedGoodValue = 1;
+            int expectedBadValue = 0;
+            string correctProductBarcode = "0017333905650";
+            string wrongProductBarcode = "1100333905650";
+            Model model = new Model();
+            string login = "Karol_Bok";
+            string password = "bok";
+            model.Login = login;
+            model.Password = password;
+            model.OpenConnection(ConnectionType.Shop);
+
+            //act
+            int good = model.ShopModel.GetProductsByBarcode(correctProductBarcode).Count;
+            int bad = model.ShopModel.GetProductsByBarcode(wrongProductBarcode).Count;
+
+
+            //assert
+            Assert.AreEqual(expectedGoodValue, good);
+            Assert.AreEqual(expectedBadValue, bad);
+        }
     }
 }
