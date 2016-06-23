72ba7bf6eb6e272d5fe70866888d4048cfa127ee
HannaHanslik <hannahanslik@gmail.com>
2016-06-23 23:54:53 +0200

    Add new class ShoppinCart
    This class will have a list of products in acctual shop, which we want
    to by. We can add new product to cart, update quantity of existing
    products and also remove product. I albo add tests for all methods in
    this class, each tests have been passed :)

---
+++ mShop/ShoppingCart.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mShop
+{
+    public class ShoppingCart
+    {
+        public List<products_in_shop> SCart { get; private set;} = new List<products_in_shop>();
+
+        public bool AddNewProduct(products_in_shop product)
+        {
+            int exist = SCart.Where(item => item.Id == product.Id).ToList().Count;
+            if (exist == 0)
+            {
+                SCart.Add(product);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool RemoveProduct(int productID)
+        {
+            foreach(var product in SCart)
+            {
+                if(product.Id == productID)
+                {
+                    SCart.Remove(product);
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+        public bool UpdateProdactQuantity(int productID, int quantity)
+        {
+            foreach(var product in SCart)
+            {
+                if(product.Id == productID)
+                {
+                    if (quantity > 0)
+                    {
+                        product.Quantity = quantity;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ mShopTests/ShoppingCartTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using mShop.Models;
+using mShop;
+
+using System.Threading.Tasks;
+
+namespace mShopTests
+{
+    [TestClass]
+    public class ShoppingCartTests
+    {
+        [TestMethod]
+        public void AddNewProduct()
+        {
+            //arrange
+            products_in_shop product = new products_in_shop();
+            product.Id = 5;
+            product.Quantity = 4;
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+ 
+            bool expectedGoodValue = true;
+            bool expectedBadValue = false;
+
+            //act
+            bool good = shoppingCart.AddNewProduct(product);
+            bool bad = shoppingCart.AddNewProduct(product);
+
+            //assert
+            Assert.AreEqual(expectedGoodValue, good);
+            Assert.AreEqual(expectedBadValue, bad);
+        }
+
+        [TestMethod]
+        public void RemoveProduct()
+        {
+            //arrange
+            products_in_shop goodProduct = new products_in_shop();
+            goodProduct.Id = 5;
+
+            products_in_shop badProduct = new products_in_shop();
+            badProduct.Id = 500;
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+
+            bool expectedGoodValue = true;
+            bool expectedBadValue = false;
+
+            shoppingCart.AddNewProduct(goodProduct);
+
+            //act
+
+            bool good = shoppingCart.RemoveProduct(goodProduct.Id);
+            bool bad = shoppingCart.RemoveProduct(badProduct.Id);
+
+            //assert
+            Assert.AreEqual(expectedGoodValue, good);
+            Assert.AreEqual(expectedBadValue, bad);
+        }
+
+        [TestMethod]
+        public void UpdateProduct()
+        {
+            //arrange
+            products_in_shop goodProduct = new products_in_shop();
+            goodProduct.Id = 5;
+            goodProduct.Quantity = 50;
+
+            products_in_shop badProduct = new products_in_shop();
+            badProduct.Id = 10;
+
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+
+            bool expectedGoodValue = true;
+            bool expectedBadValue = false;
+
+            shoppingCart.AddNewProduct(goodProduct);
+
+            //act
+
+            bool good = shoppingCart.UpdateProdactQuantity(goodProduct.Id, 15);
+            bool bad1 = shoppingCart.UpdateProdactQuantity(goodProduct.Id, -10);
+            bool bad2 = shoppingCart.UpdateProdactQuantity(badProduct.Id, 10);
+
+            //assert
+            Assert.AreEqual(expectedGoodValue, good);
+            Assert.AreEqual(expectedBadValue, bad1);
+            Assert.AreEqual(expectedBadValue, bad2);
+        }
+
+    }
+}
